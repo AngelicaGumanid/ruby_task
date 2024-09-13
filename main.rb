c8bd2f95505ade597b6924550f33cb5fbd814e92
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-13 15:47:43 +0800

    Create and array of person and print.

--- main.rb
+++ main.rb
@@ -1,1 +1,28 @@
-puts "Hello World"
+persons =
+  [
+    {name: "Ahyeon", age: 17, national_id: 00001},
+    {name: "Chastine", age: 23, national_id: 805973462},
+    {name: "Ezekielle", age: 25, national_id: 309745326},
+    {name: "Andre", age: 25, national_id: 49245678},
+    {name: "Justin", age: 25, national_id: 47836574},
+    {name: "Emmanuel", age: 25, national_id: 469549},
+    {name: "Alodia", age: 25, national_id: 4944474},
+    {name: "Camille", age: 25, national_id: 97845215},
+    {name: "Neil", age: 25, national_id: 49722657},
+    {name: "Aia", age: 25, national_id: 14864116},
+    {name: "Ruka", age: 25, national_id: 45345354},
+    {name: "Rora", age: 25, national_id: 87678673},
+    {name: "Rami", age: 25, national_id: 4123457},
+    {name: "Pharita", age: 25, national_id: 37864378},
+    {name: "Chiquita", age: 25, national_id: 783672374},
+    {name: "Asa", age: 25, national_id: 3453487},
+    {name: "Hanni", age: 25, national_id: 737783732},
+    {name: "Jedd", age: 25, national_id: 11654631},
+    {name: "Marie", age: 25, national_id: 4537837},
+    {name: "Ziana", age: 25, national_id: 737378347},
+  ]
+
+# DISPLAY ARRAY
+persons.each do |key, value|
+  puts "Information: #{key}, #{value}\n"
+end
