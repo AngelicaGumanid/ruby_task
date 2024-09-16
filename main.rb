484d209117a92ce2fd77f774bd0c536b7dd26016
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-16 15:50:31 +0800

    Availabilty of the search option (working),

--- main.rb
+++ main.rb
@@ -1,35 +1,27 @@
 # ==================== ARRAY ====================
 persons =
   [
-    { name: "Ahyeon", age: 17, national_id: 041107 },
-    { name: "Chastine", age: 23, national_id: 542902 },
-    { name: "Ezekielle", age: 25, national_id: 122901 },
-    { name: "Andre", age: 25, national_id: 012004 },
-    { name: "Justin", age: 25, national_id: 580106 },
-    { name: "Emmanuel", age: 25, national_id: 935466 },
-    { name: "Alodia", age: 25, national_id: 120545 },
-    { name: "Camille", age: 25, national_id: 057745 },
-    { name: "Neil", age: 25, national_id: 591601 },
-    { name: "Aia", age: 25, national_id: 510498 },
-    { name: "Ruka", age: 25, national_id: 012045 },
-    { name: "Rora", age: 25, national_id: 124578 },
-    { name: "Rami", age: 25, national_id: 557766 },
-    { name: "Pharita", age: 25, national_id: 870045 },
-    { name: "Chiquita", age: 25, national_id: 467977 },
-    { name: "Asa", age: 25, national_id: 760923 },
-    { name: "Hanni", age: 25, national_id: 356899 },
-    { name: "Jedd", age: 25, national_id: 236497 },
-    { name: "Marie", age: 25, national_id: 548790 },
-    { name: "Ziana", age: 25, national_id: 4578086 }
+    { name: "Ahyeon", age: 17, national_id: 41107 },
+    { name: "Chastine", age: 23, national_id: 42902 },
+    { name: "Ezekielle", age: 25, national_id: 12201 },
+    { name: "Andre", age: 25, national_id: 12004 },
+    { name: "Justin", age: 25, national_id: 58106 },
+    { name: "Emmanuel", age: 25, national_id: 93546 },
+    { name: "Alodia", age: 25, national_id: 12045 },
+    { name: "Camille", age: 25, national_id: 57745 },
+    { name: "Neil", age: 25, national_id: 59161 },
+    { name: "Aia", age: 25, national_id: 51098 },
+    { name: "Ruka", age: 25, national_id: 12045 },
+    { name: "Rora", age: 25, national_id: 12478 },
+    { name: "Rami", age: 25, national_id: 55776 },
+    { name: "Pharita", age: 25, national_id: 87045 },
+    { name: "Chiquita", age: 25, national_id: 47977 },
+    { name: "Asa", age: 25, national_id: 76923 },
+    { name: "Hanni", age: 25, national_id: 35689 },
+    { name: "Jedd", age: 25, national_id: 23697 },
+    { name: "Marie", age: 25, national_id: 54870 },
+    { name: "Ziana", age: 25, national_id: 457808 }
   ]
-
-# ==================== DISPLAY ARRAY ====================
-def display_list(persons)
-  puts "\nLIST OF NATIONAL IDs:"
-  persons.each do |person|
-    puts "National ID: #{person[:national_id]}, Name: #{person[:name]}, Age: #{person[:age]}"
-  end
-end
 
 # ==================== ADD USER LOGIC ====================
 def add_user(persons)
@@ -39,6 +31,7 @@ def add_user(persons)
 
   if persons.any? { |person| person[:national_id] == national_id }
     puts "\nFailed to add: National ID already exists."
+
   else
     print "Input name: "
     name = gets.chomp
@@ -50,11 +43,10 @@ def add_user(persons)
 
     puts "\nUser added successfully!"
 
-    system("clear")
-
   end
 
   display_list(persons)
+
 end
 
 # ==================== DELETE USER LOGIC ====================
@@ -67,11 +59,15 @@ def delete_user(persons)
 
   if index.nil?
     puts "\nUser not found."
+
   else
     persons.delete_at(index)
     print "\nSuccessfully deleted."
+
   end
+
   display_list(persons)
+
 end
 
 # ==================== SEARCH LOGIC ====================
@@ -81,35 +77,41 @@ def search_user(persons)
   search = gets.chomp
 
   if search.to_i.to_s == search
-    search = search.to_i
-    user = persons.find { |person| person[:national_id] == search }
+    find = search.to_i
+    person = persons.find { |person| person[:national_id] == find }
+
   else
     person = persons.find { |person| person[:name].downcase == search.downcase }
+
   end
 
-  if user
+  if person
     puts "\nUser found = Name: #{person[:name]}, Age: #{person[:age]}, National ID: #{person[:national_id]}"
+
   else
     puts "\nUser not found!"
+
   end
 
 end
 
-# ==================== DISPLAY LAST 5 USERS ====================
-def display_last20(persons)
-  puts "\nTHE LAST 5 PERSONS:"
-  persons.last(5).each do |person|
+# DISPLAY LIST LOGIC UPDATED AND RECENT 5
+def display_list(persons)
+  puts "\nLIST OF NATIONAL IDs:"
+
+  persons.first(5).each do |person|
     puts "National ID: #{person[:national_id]}, Name: #{person[:name]}, Age: #{person[:age]}"
   end
+
 end
 
 # ==================== MAIN ====================
 loop do
+
   puts "\n=================================================="
   puts "TYPE 'add_user' TO ADD A USER."
   puts "TYPE 'delete_user' TO DELETE A USER."
   puts "TYPE 'search_user' TO SEARCH FOR USER."
-  puts "TYPE 'display_last' TO DISPLAY LAST 20 PERSONS IN THE LIST."
   puts "TYPE 'exit' TO EXIT THE PROGRAM."
   puts "=================================================="
 
@@ -118,16 +120,20 @@ loop do
 
   if choose == "add_user"
     add_user(persons)
+
   elsif choose == "delete_user"
     delete_user(persons)
+
   elsif choose == "search_user"
     search_user(persons)
-  elsif choose == "display_last"
-    display_last20(persons)
+
   elsif choose == "exit"
     puts "\nExisting the program!"
     break
+
   else
-    puts "Invalid!"
+    puts "\nINVALID! PLEASE MAKE SURE YOU TYPE THE CORRECT SPELLING! TRY AGAIN!"
+
   end
+
 end
