945b40aac6bd8aa7b34953319439fc6d313d2812
AngelicaGumanid <angelicarose.gumanid@gmail.com>
2024-09-18 13:47:06 +0800

    Created the person class.

--- main.rb
+++ main.rb
@@ -1,26 +1,42 @@
+# ==================== PERSON CLASS ====================
+class Person
+  attr_accessor :mame, :age, :national_id
+
+ def initialize(name, age, national_id)
+ @name = name
+ @age = age
+ @national_id = national_id
+ end
+
+def to_s
+  "National ID: #{@national_id}, Name:: #{@name}, Age:: #{@age}"
+end
+end
+
 # ==================== ARRAY ====================
 persons =
   [
-    { name: "Ahyeon", age: 17, national_id: 41107 },
-    { name: "Chastine", age: 23, national_id: 42902 },
-    { name: "Ezekielle", age: 25, national_id: 12201 },
-    { name: "Andre", age: 25, national_id: 12004 },
-    { name: "Justin", age: 25, national_id: 58106 },
-    { name: "Emmanuel", age: 25, national_id: 93546 },
-    { name: "Alodia", age: 25, national_id: 12045 },
-    { name: "Camille", age: 25, national_id: 57745 },
-    { name: "Neil", age: 25, national_id: 59161 },
-    { name: "Aia", age: 25, national_id: 51098 },
-    { name: "Ruka", age: 25, national_id: 12045 },
-    { name: "Rora", age: 25, national_id: 12478 },
-    { name: "Rami", age: 25, national_id: 55776 },
-    { name: "Pharita", age: 25, national_id: 87045 },
-    { name: "Chiquita", age: 25, national_id: 47977 },
-    { name: "Asa", age: 25, national_id: 76923 },
-    { name: "Hanni", age: 25, national_id: 35689 },
-    { name: "Jedd", age: 25, national_id: 23697 },
-    { name: "Marie", age: 25, national_id: 54870 },
-    { name: "Ziana", age: 25, national_id: 457808 }
+    Person.new("Ahyeon", 17, 41107 ),
+    Person.new( "Chastine", 23, 42902 ),
+    Person.new( "Ezekielle", 25, 12201 ),
+    Person.new( "Andre", 25, 12004 ),
+    Person.new( "Justin", 25, 58106 ),
+    Person.new( "Emmanuel", 25, 93546 ),
+    Person.new( "Alodia", 25, 12045 ),
+    Person.new( "Camille", 25, 57745 ),
+    Person.new( "Neil", 25, 59161 ),
+    Person.new( "Aia", 25, 51098 ),
+    Person.new( "Ruka",25, 12045 ),
+    Person.new( "Rora", 25, 12478 ),
+    Person.new( "Rami", 25, 55776 ),
+    Person.new( "Pharita", 25, 87045 ),
+    Person.new( "Chiquita",25, 47977 ),
+    Person.new( "Asa", 25, 76923 ),
+    Person.new( "Hanni", 25, 356809 ),
+    Person.new( "Jedd",25, 23697 ),
+    Person.new( "Marie", 25, 54870 ),
+    Person.new( "Ziana", 25, 457808 )
+
   ]
 
 # ==================== ADD USER LOGIC ====================
@@ -29,7 +45,7 @@ def add_user(persons)
   print "\nInput a National ID: "
   national_id = gets.chomp.to_i
 
-  if persons.any? do |person| person[:national_id] == national_id end
+  if persons.any? { |person| person.national_id == national_id }
     puts "\nFailed to add: National ID already exists."
   else
     print "Input name: "
@@ -38,8 +54,7 @@ def add_user(persons)
     print "Input age: "
     age = gets.chomp.to_i
 
-    persons.unshift({ name: name, age: age, national_id: national_id })
-
+    persons.unshift(Person.new(name, age, national_id ))
     puts "\nUser added successfully!"
   end
 
@@ -53,7 +68,7 @@ def delete_user(persons)
   print "\nEnter National ID to delete: "
   national_id = gets.chomp.to_i
 
-  index = persons.find_index { |person| person[:national_id] == national_id }
+  index = persons.find_index { |person| person.national_id == national_id }
 
   if index
     puts "\nUser not found."
@@ -73,13 +88,13 @@ def search_user(persons)
   search = gets.chomp
 
   person = if search =~ /^\d+$/
-    persons.find { |person| person[:national_id] == search.to_i }
+    persons.find { |person| person.national_id == search.to_i }
   else
-    persons.find { |person| person[:name].casecmp ==(search).zero? }
+    persons.find { |person| person.name.casecmp == (search).zero? }
   end
 
   if person
-    puts "\nUser found = Name: #{person[:name]}, Age: #{person[:age]}, National ID: #{person[:national_id]}"
+    puts "\nUser found = Name: #{person.name}, Age: #{person.age}, National ID: #{person.national_id}"
   else
     puts "\nUser not found!"
   end
@@ -93,32 +108,32 @@ def edit_user(persons)
   print "\nEnter National ID of the user to edit: "
   national_id = gets.chomp.to_i
 
-  user_index = persons.find_index { |person| person[:national_id] == national_id}
+  user_index = persons.find_index { |person| person.national_id == national_id}
 
   if user_index
     user = persons[user_index]
 
     puts "\n=================================================="
-    print "\nEDIT USER DETAILS \nNAME:#{user[:name]} \nAGE:#{user[:age]}\n"
+    print "\nEDIT USER DETAILS \nNAME:#{user.name} \nAGE:#{user.age}\n"
     puts "\n=================================================="
 
     print "\nEnter new name (or press Enter to keep '#{user[:name]}}'): "
     new_name = gets.chomp
-    new_name = new_name ? user[:name] : new_name
+    new_name = new_name ? user.name : new_name
 
     print "Enter new age (or press Enter to keep '#{user[:age]}'): "
     input_age = gets.chomp.to_i
-    new_age = input_age ? user[:age] : input_age.to_i
+    new_age = input_age ? user.age : input_age.to_i
 
-    user[:name] = new_name
-    user[:age] = new_age
+    user.name = new_name
+    user.age = new_age
     persons.delete_at(user_index)
     persons.unshift(user)
 
     puts "\n=================================================="
     puts "\nUSER UPDATED SUCCESSFULLY"
-    print "NAME:#{user[:name]}\n"
-    print "AGE:#{user[:age]}"
+    print "NAME:#{user.name}\n"
+    print "AGE:#{user.age}"
     puts "\n=================================================="
 
   else
@@ -135,7 +150,7 @@ def display_list(persons)
   puts "\nLIST OF NATIONAL IDs:"
 
   persons.first(5).each do |person|
-    puts "National ID: #{person[:national_id]}, Name: #{person[:name]}, Age: #{person[:age]}"
+    puts person
   end
 
 end
@@ -156,22 +171,20 @@ loop do
   print "What would you like to do, choose above? "
   choose = gets.chomp
 
-  if choose == "add_user"
+  case choose
+  when "add_user"
     add_user(persons)
-  elsif choose == "delete_user"
+  when "delete_user"
     delete_user(persons)
-  elsif choose == "search_user"
+  when "search_user"
     search_user(persons)
-  elsif choose == "edit_user"
+  when "edit_user"
     edit_user(persons)
-  elsif choose == "exit"
+  when "exit"
     print "\nAre you sure you want to exit (y/n)? "
     action = gets.chomp
-
-    if action == "y"
-      puts "\nEXISTING THE PROGRAM!"
-      break
-    end
+    puts "\nEXISTING THE PROGRAM!"
+    break if action.downcase == y
   else
     puts "\nINVALID! PLEASE MAKE SURE YOU TYPE THE CORRECT SPELLING! TRY AGAIN!"
   end
